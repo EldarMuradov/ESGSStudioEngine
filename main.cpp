c4bd43d50f4631980a3a565253d7c74c2633194b
Eldar Muradov <101048266+EldarMuradov@users.noreply.github.com>
2023-02-20 23:48:39 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "AppWindow.h"
+#include "InputSystem.h"
+#include <exception>
+#include "PhysicsEngine.h"
+
+int main()
+{
+	try
+	{
+		GraphicsEngine::create();
+		InputSystem::create();
+		PhysicsEngine::create();
+	}
+	catch (...)
+	{
+		throw std::exception("Failed to create {GraphicsEngine} / {InputSystem} / {PhysicsEngine}. Erorr in {main()}.");
+		return -1;
+	}
+
+	try
+	{
+		AppWindow app;
+		while (app.isRun());
+	}
+	catch (...)
+	{
+		throw std::exception("Failed to create a frame. Erorr in {AppWindow} pipeline.");
+		InputSystem::release();
+		GraphicsEngine::release();
+		PhysicsEngine::release();
+		return -1;
+	}
+	
+	InputSystem::release();
+	GraphicsEngine::release();
+	PhysicsEngine::release();
+
+	return 0;
+}
